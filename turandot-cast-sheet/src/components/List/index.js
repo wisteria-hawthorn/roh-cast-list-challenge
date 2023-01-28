b34436ca83b39dfe77a98718381fe5099711e6b3
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2023-01-28 15:04:16 +0000

    tidied up fetch requests and the checkTags function

--- turandot-cast-sheet/src/components/List/index.js
+++ turandot-cast-sheet/src/components/List/index.js
@@ -1,21 +1,24 @@
 // LIST: this will be a reusable unordered list component, capable of displaying any fetched data necessary, and allowing the Cast Sheet to be scalable if any additional lists need to be added in the future.
 
 import React from 'react';
+import ListItem from '../ListItem';
 
 
-export default function List() {
+export default function List({ListItem}) {
 
-/* LIST: probably a .map array method to render as many <li> as needed in the JSON object.
+/* LIST: 
+- .map to render as many <li> (ListItem) as needed
 
 ALSO CONSIDER:
-- if two results contain the same "role" value (e.g. Libretto), render them together with a comma?
+// - if two results contain the same "role" value (e.g. Libretto), render them together with a comma?
 
 */
 
+// WIP, testing:
 return(
     <div className="ul-container">
         <ul>
-            <li>result1 (role)</li>
+            <li>{ListItem}</li>
             <li>result2 (role)</li>
             <li>result3 (role)</li>
         </ul>
--- turandot-cast-sheet/src/components/Sheet/index.js
+++ turandot-cast-sheet/src/components/Sheet/index.js
@@ -3,50 +3,75 @@
 import React , { useState , useEffect } from "react";
 import List from "../List";
 
+import './style.css';
+
 function Sheet() { 
 
 
-// FETCH REQUEST
-        const [data , setData] = useState(null);
+// State management:
+        const [ title , setTitle ] = useState("");
+        const [ date , setDate ] = useState("");
+        let [ descr , setDescr ] = useState("")
 
+        const [ creativeName , setCreativeName ] = useState("");
+        const [ creativeRole , setCreativeRole ] = useState("");
+
+
+        // Removing any <p></p> tags present in the JSON data:
+        function checkTags(string) {
+            if (string.includes("<p>")) {
+                return string.slice(3, -4);
+            } else {
+                return string;
+            }};
+
+        // FETCH REQUEST:    
         useEffect( () => {
             async function fetchData() {
-            const response = await fetch (`https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban`);
-            const {data} = await response.json();
-
-            setData(data);
+            const response = await fetch (`https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban` , { headers: {
+                'Accept': 'application/json'
+            }
+              });
+            const data = await response.json();
+            console.log(data);
             
-            }    
+            // variable to allow access to the nested date value: TODO: sometimes shows in correct format, sometimes doesn't? 
+            // const theArrayIndex = 14;
+
+            setTitle(checkTags(data.data.attributes.title));
+            setDate(data.included[14].attributes.date);
+            console.log(`THIS IS THE DATE: ${date}`);
+            setDescr(checkTags(data.data.attributes.shortDescription))
+
+
+            setCreativeName(checkTags(data.included[1].attributes.name))
+            setCreativeRole(checkTags(data.included[1].attributes.role))
+
+            }  
+
             fetchData()
             .catch(console.error);
 
         //eslint-disable-next-line
         },[])
         
-        // Displays loading status whilst the fetch function is being called
-        if(!data){
-            return <p>Loading...</p>
+        // Displays loading status whilst the fetch function is being called.
+        if(!title){
+            return <div className="main-sheet-container"><p className="loading">Loading...</p></div>
         };
-        
-        console.log(data);
-
-
-    // Storing the fetched data in variables
-        const prodTitle = data.attributes.title;
-        const prodDescr = data.attributes.shortDescription;
-
-        const prodDate = "10/03/2023";
-
+       
+         
 
     return (
         <div className="main-sheet-container">
 
             <section className="prod-info-container">
 
-                <h1 className="prod-title-heading">{prodTitle}</h1>
+                <h1 className="prod-title-heading">{title}</h1>
 
-                <p className="prod-date">Date: {prodDate}</p>
-                <p className="prod-descr">{prodDescr}</p>
+                <p className="prod-date">Date: {date}</p>
+                <p className="prod-descr">{descr}</p>
+                
 
             </section>
 
@@ -63,6 +88,12 @@ function Sheet() {
             <h2 className="cast-heading">Cast</h2>
 
                 <List className="cast-list"/>
+
+                <hr></hr>
+
+                <p>FETCH CHECK: creatives</p>
+                <p>{creativeName}</p>
+                <p><span>{creativeRole}</span></p>
 
             </section>
 
